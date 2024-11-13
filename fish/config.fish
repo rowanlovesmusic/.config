2a7ea815526ca82b14a1851c21eca83b8228a72f
rowanlovesmusic <rowanmusic13.gmail.com>
2024-11-13 09:40:14 +0100

    initial commit

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,10 @@ end
 # removes the mapping <C-t> which is being used to close the terminal in NeoVim
 bind --erase --all \ct
 
+alias vim="nvim"
+alias rm="rm -I"
+source ~/.pyvenv/bin/activate.fish
+
 # fish is aware of the paths set by brew:
 # to ensure that brew paths are recognized inside fish, run:
 #    /opt/brew/bin/brew shellenv >> ~/.config/fish/config.fish 
@@ -13,17 +17,19 @@ fish_config prompt choose scales
 
 # set -x TERM tmux-256color
 
-# # modify the prompt
-# function fish_prompt
-#     string join '' -- $PWD '>'
-# end
-
-# runs zoxide if installed
-if type -q zoxide
-zoxide init fish --cmd cd | source
-end
+# brew paths
+set --global --export HOMEBREW_PREFIX "/opt/homebrew";
+set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
+fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
+if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
 
 # runs neofetch if installed
-if type -q neofetch
-neofetch
-end
+# if type -q neofetch
+# 	neofetch
+# end
+
+# init zoxide if installed
+fish_add_path /opt/homebrew/bin/zoxide
+zoxide init fish | source
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,19 @@
 require("neotex.core")
 require("neotex.bootstrap")
+
+vim.cmd("language en_US")
+vim.opt.spelllang = "en,it"
+vim.cmd("set foldenable")
+vim.opt.foldmethod = "manual"
+-- vim.cmd("autocmd BufWinLeave *.* mkview")
+-- vim.cmd("autocmd BufWinEnter *.* silent loadview")
+vim.cmd[[
+  " Use Tab to expand and jump through snippets
+  imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+  smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+  " Use Shift-Tab to jump backwards through snippets
+  imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+  smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/luasnip/"})
